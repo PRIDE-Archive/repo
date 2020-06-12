b514d13b11428843093ff7c119143c30c3d17dc5
Chakradhar Bandla <chakrabandla@gmail.com>
2020-06-12 01:16:59 +0100

    improve project service

--- src/main/java/uk/ac/ebi/pride/archive/repo/services/project/ProjectService.java
+++ src/main/java/uk/ac/ebi/pride/archive/repo/services/project/ProjectService.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.pride.archive.repo.services.project;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Rui Wang
@@ -12,5 +13,14 @@ public interface ProjectService {
 
   ProjectSummary findByAccession(String accession) throws ProjectAccessException;
 
+  List<ProjectSummary> findFilteredBySubmitterIdAndIsPublic(Long submitterId, Boolean isPublic) throws ProjectAccessException;
+
+  List<ProjectSummary> findFilteredByReviewer(String user_aap_ref) throws ProjectAccessException;
+
   Collection<ProjectSummary> findAllBySubmitterId(Long submitterId) throws ProjectAccessException;
+
+  List<List<String>> findMonthlySubmissions();
+
+  List<String> findAllAccessionsChanged();
+
 }
